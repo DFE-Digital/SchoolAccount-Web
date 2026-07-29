6a6c430f3cf47b8dde380d7d5f967c829b5456a7
Kian Winstanley <Kian.WINSTANLEY@EDUCATION.GOV.UK>
2026-07-29 11:44:12 +0100

    Remove Clean Architecture related work and associated tests

--- src/SchoolAccount.Application/Organisations/GetByLaestab/GetOrganisationByLaestabQuery.cs
+++
@@ -1,5 +0,0 @@
-using SchoolAccount.Application.Abstractions.Messaging;
-
-namespace SchoolAccount.Application.Organisations.GetByLaestab;
-
-public sealed record GetOrganisationByLaestabQuery(string laestab) : IQuery<OrganisationResponse>;
