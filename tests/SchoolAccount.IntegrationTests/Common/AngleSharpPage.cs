836fd75f78e00b291744b8f59922443d362add9d
KianW-DfE <kian.winstanley@education.gov.uk>
2026-07-31 16:11:00 +0100

    Add basic error handling (#13)
    Add 404 Not Found, 403 Forbidden and 500 Error pages
    Add common AngleSharp methods for testing
    
    SAB-228
    
    Co-authored-by: Paul Custance <paul.custance@education.gov.uk>
    Co-authored-by: andycunningham <andy.cunningham@education.gov.uk>

---
+++ tests/SchoolAccount.IntegrationTests/Common/AngleSharpPage.cs
@@ -0,0 +1,35 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+
+namespace SchoolAccount.IntegrationTests.Common;
+
+public class AngleSharpPage
+{
+    private readonly IDocument _page;
+
+    public AngleSharpPage(string pageContent)
+    {
+        IBrowsingContext context = BrowsingContext.New(Configuration.Default);
+        _page = context
+            .OpenAsync(req => req.Content(pageContent), TestContext.Current.CancellationToken)
+            .Result;
+    }
+
+    public string? GetTitle()
+    {
+        IElement? pageTitle = _page.QuerySelector("title");
+        return pageTitle?.TextContent;
+    }
+
+    public string? GetFirstHeading()
+    {
+        IElement? headingElement = _page.QuerySelector("h1.govuk-heading-l");
+        return headingElement?.TextContent;
+    }
+
+    public string? GetFirstBody()
+    {
+        IElement? bodyElement = _page.QuerySelector("body");
+        return bodyElement?.TextContent;
+    }
+}
