2d47eab928357f7a7487c123eb433e85688f2574
christopherkelly <chris.kelly@education.gov.uk>
2026-08-13 12:38:31 +0100

    Clean up ErrorControllerTests and add a few more to ensure 404's are authetnicated too continueing that secure by design principle

--- tests/SchoolAccount.IntegrationTests/Features/Error/ErrorControllerTests.cs
+++ tests/SchoolAccount.IntegrationTests/Features/Error/ErrorControllerTests.cs
@@ -13,7 +13,8 @@ namespace SchoolAccount.IntegrationTests.Features.Error;
 
 public class ErrorControllerTests : IClassFixture<SchoolAccountWebApplicationFactory<Program>>
 {
-    private readonly HttpClient _client;
+    private readonly SchoolAccountWebApplicationFactory<Program> _factory;
+    private readonly HttpClient _authenticatedClient;
 
     private readonly IQueryHandler<
         GetTimeSpecificHelloQuery,
@@ -24,7 +25,8 @@ public class ErrorControllerTests : IClassFixture<SchoolAccountWebApplicationFac
 
     public ErrorControllerTests(SchoolAccountWebApplicationFactory<Program> factory)
     {
-        _client = factory.CreateAuthorisedClient(services =>
+        _factory = factory;
+        _authenticatedClient = factory.CreateAuthorisedClient(services =>
             services.AddScoped<
                 IQueryHandler<GetTimeSpecificHelloQuery, GetTimeSpecificHelloResponse>
             >(_ => _getTimeSpecificHelloHandler)
@@ -35,9 +37,13 @@ public class ErrorControllerTests : IClassFixture<SchoolAccountWebApplicationFac
     public async Task Ensure_that_the_not_found_http_status_has_corresponding_page()
     {
         // Arrange
-        var response = await _client.GetAsync("/error/404", TestContext.Current.CancellationToken);
+        var requestUri = _factory.GeneratePath("Error", "Error", new { statusCode = 404 });
 
         // Act
+        var response = await _authenticatedClient.GetAsync(
+            requestUri,
+            TestContext.Current.CancellationToken
+        );
         var page = await AngleSharpPage.FromResponseAsync<ErrorPage>(
             response,
             TestContext.Current.CancellationToken
@@ -55,9 +61,13 @@ public class ErrorControllerTests : IClassFixture<SchoolAccountWebApplicationFac
     public async Task Ensure_that_the_internal_server_error_http_status_has_corresponding_page()
     {
         // Arrange
-        var response = await _client.GetAsync("/error/500", TestContext.Current.CancellationToken);
+        var requestUri = _factory.GeneratePath("Error", "Error", new { statusCode = 500 });
 
         // Act
+        var response = await _authenticatedClient.GetAsync(
+            requestUri,
+            TestContext.Current.CancellationToken
+        );
         var page = await AngleSharpPage.FromResponseAsync<ErrorPage>(
             response,
             TestContext.Current.CancellationToken
@@ -70,38 +80,71 @@ public class ErrorControllerTests : IClassFixture<SchoolAccountWebApplicationFac
         response.StatusCode.ShouldBe(HttpStatusCode.InternalServerError);
     }
 
-    // Test to ensure that the server error page triggers correctly
-    // [Fact]
-    // public async Task Ensure_that_the_server_error_http_status_has_corresponding_page()
-    // {
-    //     // Arrange
-    //     var response = await _client.GetAsync(
-    //         "/error/500",
-    //         TestContext.Current.CancellationToken
-    //     );
-    //
-    //     var requestUri = factory.GeneratePath<DashboardController>(
-    //         nameof(DashboardController.Dashboard)
-    //     );
-    //
-    //     _getTimeSpecificHelloHandler
-    //         .Handle(Arg.Any<GetTimeSpecificHelloQuery>(), Arg.Any<CancellationToken>())
-    //         .Throws(new ApplicationException("Bang!"));
-    //
-    //     // Act
-    //     var response = await _client.GetAsync(requestUri, TestContext.Current.CancellationToken);
-    //
-    //     var html = await response.Content.ReadAsStringAsync(TestContext.Current.CancellationToken);
-    //     var page = new AngleSharpPage(html);
-    //
-    //     // Assert
-    //     page.ShouldNotBeNull();
-    //
-    //     var pageTitle = page.GetTitle();
-    //     pageTitle.ShouldNotBeNull();
-    //     pageTitle.ShouldBeEquivalentTo(ErrorViewModel.ErrorTitle);
-    //
-    //     response.IsSuccessStatusCode.ShouldBeFalse();
-    //     response.StatusCode.ShouldBe(HttpStatusCode.InternalServerError);
-    // }
+    [Fact]
+    public async Task Ensure_that_if_someone_is_navigating_to_any_page_valid_or_invalid_it_will_try_to_authenticate_them()
+    {
+        var client = _factory.CreateUnauthorisedClient();
+        var requestUri = "/this-page-does-not-exist";
+
+        // Act
+        var response = await client.GetAsync(requestUri, TestContext.Current.CancellationToken);
+
+        // Assert
+        response.IsSuccessStatusCode.ShouldBeFalse();
+        response.StatusCode.ShouldBe(HttpStatusCode.Redirect);
+    }
+
+    [Fact]
+    public async Task If_someone_is_authenticated_trying_to_access_a_unknown_page_they_will_get_a_not_found_page()
+    {
+        // Arrange
+        var requestUri = "/this-page-does-not-exist";
+
+        // Act
+        var response = await _authenticatedClient.GetAsync(
+            requestUri,
+            TestContext.Current.CancellationToken
+        );
+        var page = await AngleSharpPage.FromResponseAsync<ErrorPage>(
+            response,
+            TestContext.Current.CancellationToken
+        );
+
+        // Assert
+        page.ShouldNotBeNull();
+        page.IsNotFoundPageTitle().ShouldBeTrue();
+        page.IsNotFoundPageHeading().ShouldBeTrue();
+        response.IsSuccessStatusCode.ShouldBeFalse();
+        response.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+    }
+
+    [Fact]
+    public async Task Ensure_that_the_server_error_http_status_has_corresponding_page()
+    {
+        // Arrange
+        var requestUri = _factory.GeneratePath("Dashboard", "Dashboard");
+        _getTimeSpecificHelloHandler
+            .Handle(Arg.Any<GetTimeSpecificHelloQuery>(), Arg.Any<CancellationToken>())
+            .Throws(new ApplicationException("Bang!"));
+
+        // Act
+        var response = await _authenticatedClient.GetAsync(
+            requestUri,
+            TestContext.Current.CancellationToken
+        );
+        var page = await AngleSharpPage.FromResponseAsync<CommonPage>(
+            response,
+            TestContext.Current.CancellationToken
+        );
+
+        // Assert
+        page.ShouldNotBeNull();
+
+        var pageTitle = page.GetTitle();
+        pageTitle.ShouldNotBeNull();
+        pageTitle.ShouldBeEquivalentTo(ErrorViewModel.ErrorTitle);
+
+        response.IsSuccessStatusCode.ShouldBeFalse();
+        response.StatusCode.ShouldBe(HttpStatusCode.InternalServerError);
+    }
 }
