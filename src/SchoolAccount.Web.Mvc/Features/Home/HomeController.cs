76cd526b5cd8c1363d83500ea13ed829cc23fe21
andycunningham <andy.cunningham@education.gov.uk>
2026-08-14 15:22:19 +0100

    Update header to align with designs

--- src/SchoolAccount.Web.Mvc/Features/Home/HomeController.cs
+++ src/SchoolAccount.Web.Mvc/Features/Home/HomeController.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,7 +6,8 @@ namespace SchoolAccount.Web.Mvc.Features.Home;
 [Route("/")]
 public class HomeController : Controller
 {
-    [HttpGet(""), AllowAnonymous]
+    [HttpGet("")]
+    [AllowAnonymous]
     public async Task<IActionResult> Home()
     {
         if (User.Identity?.IsAuthenticated ?? false)
