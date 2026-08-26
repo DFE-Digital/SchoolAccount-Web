d36c9f4fe461d3a3bc8dfb612254563bf7b1b097
Paul Custance <paul.custance@education.gov.uk>
2026-08-26 18:20:31 +0100

    Variable name should reflect actual content

--- src/SchoolAccount.Web.Mvc/Features/Dashboard/DashboardViewModel.cs
+++ src/SchoolAccount.Web.Mvc/Features/Dashboard/DashboardViewModel.cs
@@ -1,3 +1,3 @@
 namespace SchoolAccount.Web.Mvc.Features.Dashboard;
 
-public record DashboardViewModel(string User, List<string> CensusGreetings);
+public record DashboardViewModel(string User, List<string> CensusStatuses);
