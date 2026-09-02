84081fd77debdf04533e311e0d7b3a6611dbfbb5
andycunningham <andy.cunningham@education.gov.uk>
2026-09-02 10:52:25 +0100

    Moved Unknown user logic into the journey view model

--- src/SchoolAccount.Web.Mvc/Features/Journey/JourneyViewModel.cs
+++ src/SchoolAccount.Web.Mvc/Features/Journey/JourneyViewModel.cs
@@ -2,7 +2,12 @@ namespace SchoolAccount.Web.Mvc.Features.Journey;
 
 public sealed class JourneyViewModel
 {
-    public string User { get; init; }
     public Uri CallToActionUrl { get; init; }
     public string CallToActionButtonText { get; init; }
+
+    public string User
+    {
+        get;
+        init => field = value ?? "Unknown";
+    } = "Unknown";
 }
