9372c613c3db39d51c4ae79a991ed565f1e734e5
andycunningham <andy.cunningham@education.gov.uk>
2026-09-02 12:27:54 +0100

    Make button name more generic in AngleSharpPage.cs

--- src/SchoolAccount.Web.Mvc/Features/Journey/JourneyViewModel.cs
+++ src/SchoolAccount.Web.Mvc/Features/Journey/JourneyViewModel.cs
@@ -2,7 +2,7 @@ namespace SchoolAccount.Web.Mvc.Features.Journey;
 
 public sealed class JourneyViewModel
 {
-    public string User { get; init; } = "Unknown";
+    public string? User { get; init; } = "Unknown";
     public Uri CallToActionUrl { get; init; }
     public string CallToActionButtonText { get; init; }
 }
